0c362e25434c6813ad9f507aae36a4145e3c269d
sam111sam <samuelnega1@gmail.com>
2024-08-19 03:15:08 +0000

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python3
-"""Function to retrieve the subscriber count for a specific Reddit subreddit."""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 def number_of_subscribers(subreddit):
-    """Fetch and return the total number of subscribers for the provided subreddit."""
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/Immediate-Piano3234)"
+        "User-Agent": "my_reddit_app/1.0 (by /u/Immediate-Piano3234)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
+    
     if response.status_code == 404:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    elif response.status_code != 200:
+        ''' print("Unexpected status code: {}".format(response.status_code))'''
+        return 0
+    
+    # Ensure response encoding is correct
+    response.encoding = 'utf-8'
+    
+    try:
+        data = response.json().get("data")
+        return data.get("subscribers")
+    except ValueError:
+        print("Failed to decode JSON from response.")
+        return 0
 
