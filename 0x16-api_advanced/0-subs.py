c1316e77b91c8da9d45ff3166a7b0178350cb2b7
sam111sam <samuelnega1@gmail.com>
2024-08-19 02:47:20 +0000

    task 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ def number_of_subscribers(subreddit):
     """Fetch and return the total number of subscribers for the provided subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/Immediate-Piano3234)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
